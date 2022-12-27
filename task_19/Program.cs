0aec2cddaae1f3f1a866d0452e6ab0ad43cee1f1
Sergey <aseev-1977@list.ru>
2022-12-27 18:14:36 +0300

    task_19

---
+++ task_19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+string num = Console.ReadLine();
+
+if (num.Length == 5)
+{
+    if (num[0]==num[4] && num[1]==num[3])
+    {
+       Console.WriteLine($"число {num} является палиндромом");
+    }
+    else
+    {
+       Console.WriteLine($"число {num} не является палиндромом");
+    }
+}
+ else
+{
+    Console.WriteLine("Ошибка, (введите пятизначное число) :");
+}
